e1255ecb8b1d64412a2d59b3e387aa6f0fe30c8e
Vadim Drobinin <vadim@drobinin.com>
2013-03-03 20:18:27 +0400

    First version

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Fanfic:
     def __init__(self, main_url):
         self.main = main_url
         self.author = ''
+        self.name = ''
         self.title = ''
         self.genre = ''
         self.rate = ''
@@ -19,8 +20,13 @@ class Fanfic:
         html = urlopen(self.main).read().decode('utf8')
         page = fromstring(html)
         page.make_links_absolute(URL)
-        #1. определить количество глав
-        #2. определить параметры фика
+        tree = etree.parse(page)
+        tmp = tree.xpath('//*[@id="gui_table1i"]/tbody/tr[1]/td/b')
+        self.name = tmp.text
+        tmp = tree.xpath('//*[@id="gui_table1i"]/tbody/tr[1]/td/a[1]')
+        self.author = tmp.text
+        tmp = tree.xpath('//*[@id="chap_select"]')
+        self.chapters = len(tmp)
         count = 0
         while count < page.chapters:
             chapter = self.parse_page(count)
@@ -31,15 +37,20 @@ class Fanfic:
         html = urlopen(self.main + '/%d/' % count).read().decode('utf8')
         page = fromstring(html)
         page.make_links_absolute(URL)
-        table = page.iter(tag="table")[0]
-        #1. Извлечь из таблицы текст
+        tree = etree.parse(page)
+        text = ''
+        tmp = tree.xpath('//*[@id="storytext"]')
+        for p in tmp.iter(tag="p"):
+            text.append(p.text + '\n')
         return text
-    
+
     def create_txt(self):
         return self.get_title() + '\n' + '\n\n'.join(self.pages)
 
     def create_pdf(self):
-        pass
+        text = self.create_txt()
+        #1. Сгенерировать pdf
+        return pdf
 
     def get_creater(self, output_type="txt"):
         types = dict(txt=create_txt, pdf=create_pdf)
@@ -50,5 +61,11 @@ def main():
     main_url = argv[1]
     fic = Fanfic(main_url)
     fic.parse_fanfic()
-    output = fic.get_creater(argv[2])
-    #1. Сохранить output
+    res = fic.get_creater(argv[2])
+    if argv[2] == 'txt':
+        #1. Сохранить
+        pass
+
+
+if __name__ == '__main__':
+    main()
